9c2eefa80b3a2a321bbfb86fa231737b43cd2770
Juan Ignacio Castelli <67402441+JuaniCaste23@users.noreply.github.com>
2021-06-20 22:33:15 -0300

    Update Evaluacion.sql

--- Evaluacion.sql
+++ Evaluacion.sql
@@ -4,8 +4,7 @@ SELECT d.Nombre as 'Distribuidora',v.Nombre as 'Videojuego distribuido' from Dis
 INNER JOIN  Videojuego as v
 on d.id_distribuidora = v.id_distribuidora
 
-
-/* Todas las compras de una persona */
+/* Todas las compras de una persona, agrego los datos y después hago el JOIN para ver el las compras de esa persona */
 insert into Compra (id_persona,id_videojuego,fecha_de_compra)
 values ('3','5','2019-11-23')
 insert into Compra (id_persona,id_videojuego,fecha_de_compra)
@@ -17,16 +16,11 @@ values ('3','3','2019-11-23')
 insert into Compra (id_persona,id_videojuego,fecha_de_compra)
 values ('3','10','2019-11-23')
 
-select * from Compra
-select * from Persona
-select * from Videojuego
-
 select p.Nombre as 'COMPRADOR',v.Nombre as 'Juego Comprado', v.precio,c.fecha_de_compra from Videojuego as v
 INNER JOIN Compra as c 
 ON v.id_videojuego = c.id_videojuego
 INNER JOIN Persona as p
 ON c.id_persona = p.id_persona WHERE c.id_persona = 3
-
 
 /* Nombre de la distribuidora que comienze con P */
 
@@ -35,23 +29,12 @@ WHERE Nombre like 'P%'
 
 select Nombre from Distribuidora
 
-/* nombre del articulo + nombre de la distribuidora */
+/* Nombre del articulo + nombre de la distribuidora */
 
 SELECT d.nombre as 'Distribuidora',a.nombre as 'Articulo distribuido' from distribuidora as d  
 JOIN articulo as a
 on d.id_distribuidora = a.id_distribuidora
 
-/*calcular el promedio de los precios*/
+/* Calcular el promedio de los precios*/
 select avg (precio) as 'promedio'
 from articulo
-
-
-
-
-
-
-
-
-
-
-
